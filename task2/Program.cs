b179627012039b79a6c6715e10e786205c7dd1cc
Gregorian1489 <santijago@mail.ru>
2022-11-12 12:28:19 +0400

    ADD TASK 2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+
+if (namber1 > namber2)
+{
+    Console.WriteLine("Первое число больше");
+}
+else if (namber2 > namber1)
+{
+    Console.WriteLine("Второе число больше");
+}
+else if (namber2 == namber1)
+{
+    Console.WriteLine("Числа равны");
+}
