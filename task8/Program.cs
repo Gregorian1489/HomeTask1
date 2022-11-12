6f9a5aab222728ce432b059b2aa06b14e622397c
Gregorian1489 <santijago@mail.ru>
2022-11-12 13:25:13 +0400

    ADD  TASK8

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = 0;
+
+while (number1 < number-1)
+{
+    number1 =  number1+2;
+    Console.Write($"{number1} ");
+} 
+if (number <= 0)
+{
+    Console.WriteLine("По условиям задачи нужно только положительное число");
+}
