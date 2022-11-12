9adb9b2b2312156e73bc79259fa97e092143c4f2
Gregorian1489 <santijago@mail.ru>
2022-11-12 12:49:03 +0400

    ADD VERSION2 TASK2

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,11 +5,11 @@ int namber2 = Convert.ToInt32(Console.ReadLine());
 
 if (namber1 > namber2)
 {
-    Console.WriteLine("Первое число больше");
+    Console.WriteLine($"Max = {namber1}, Min = {namber2}");
 }
 else if (namber2 > namber1)
 {
-    Console.WriteLine("Второе число больше");
+    Console.WriteLine($"Max = {namber2}, Min = {namber1}");
 }
 else if (namber2 == namber1)
 {
